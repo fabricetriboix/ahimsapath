7c8d0891c62925c2e469203f528c929b3cce6561
Fabrice Triboix <ft+github-saber@falcon1.ltd>
2025-12-13 10:06:42 +0000

    Fix search bug

--- js/search.js
+++ js/search.js
@@ -4,10 +4,11 @@
 
     if (results.length) { // Are there any results?
       var appendString = '';
+      var baseUrl = window.siteBaseUrl || '';
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+        appendString += '<li><a href="' + baseUrl + item.url + '"><h3>' + item.title + '</h3></a>';
         appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
       }
 
